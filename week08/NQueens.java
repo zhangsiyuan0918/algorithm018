e153c85ac555527ef2dd45e66d0dcb13abec842e
zhangsiyuan0918 <66421398+zhangsiyuan0918@users.noreply.github.com>
2020-12-13 21:56:44 +0800

    Signed-off-by: zhangsiyuan0918 <66421398+zhangsiyuan0918@users.noreply.github.com>

---
+++ week08/NQueens.java
@@ -0,0 +1,100 @@
+// 51 N 皇后
+// n-queens
+
+//n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。 
+//
+// 
+//
+// 上图为 8 皇后问题的一种解法。 
+//
+// 给定一个整数 n，返回所有不同的 n 皇后问题的解决方案。 
+//
+// 每一种解法包含一个明确的 n 皇后问题的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。 
+//
+// 
+//
+// 示例： 
+//
+// 输入：4
+//输出：[
+// [".Q..",  // 解法 1
+//  "...Q",
+//  "Q...",
+//  "..Q."],
+//
+// ["..Q.",  // 解法 2
+//  "Q...",
+//  "...Q",
+//  ".Q.."]
+//]
+//解释: 4 皇后问题存在两个不同的解法。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 皇后彼此不能相互攻击，也就是说：任何两个皇后都不能处于同一条横行、纵行或斜线上。 
+// 
+// Related Topics 回溯算法 
+// 👍 687 👎 0
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        char[][] chess = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                chess[i][j] = '.';
+            }
+        }
+        List<List<String>> res = new ArrayList<>();
+        backtrack(0, res, chess);
+        return res;
+    }
+    private void backtrack(int row, List<List<String>> res, char[][] chess) {
+        if (row == chess.length) {
+            res.add(construct(chess));
+            return;
+        }
+        for (int col = 0; col < chess.length; col++) {
+            if (valid(col, row, chess)) {
+                chess[row][col] = 'Q';
+                backtrack(row + 1, res, chess);
+                chess[row][col] = '.';
+            }
+        }
+    }
+    private boolean valid(int col, int row, char[][] chess) {
+        for (int i = 0; i < row; i++) {
+            if (chess[i][col] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (chess[i][j] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < chess.length; i--, j++) {
+            if (chess[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+    private List<String> construct(char[][] chess) {
+        List<String> path = new ArrayList<>();
+        for (int i = 0; i < chess.length; i++) {
+            path.add(new String(chess[i]));
+        }
+        return path;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
---
+++ week08/ValidAnagram.java
@@ -0,0 +1,41 @@
+// 242 有效的字母异位词
+// valid-anagram
+//给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+// 示例 1:
+// 输入: s = "anagram", t = "nagaram"
+//输出: true
+// 示例 2:
+// 输入: s = "rat", t = "car"
+//输出: false 
+// 说明:
+//你可以假设字符串只包含小写字母。 
+// 进阶:
+//如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？ 
+// Related Topics 排序 哈希表 
+// 👍 314 👎 0
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) - 1);
+            if (map.get(c) < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
