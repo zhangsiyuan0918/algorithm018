f9cf89d1f75965392e3b736edc420da4faa056f6
zhangsiyuan0918 <66421398+zhangsiyuan0918@users.noreply.github.com>
2020-12-20 20:41:14 +0800

    第九周作业

---
+++ week09/FirstUniqueCharacterInAString.java
@@ -0,0 +1,31 @@
+// 387 字符串中的第一个唯一字符
+// first-unique-character-in-a-string
+
+//给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。 
+//
+// 
+//
+// 示例： 
+//
+// s = "leetcode"
+//返回 0
+//
+//s = "loveleetcode"
+//返回 2
+// 
+//
+// 
+//
+// 提示：你可以假定该字符串只包含小写字母。 
+// Related Topics 哈希表 字符串 
+// 👍 297 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int firstUniqChar(String s) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
